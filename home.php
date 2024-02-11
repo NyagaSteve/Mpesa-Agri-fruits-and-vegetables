6659210b8c53fd99acc079832a44038c53a3dae8
Thiongo Stephen Nyaga <stephennyaga045@gmail.com>
2024-02-12 00:15:32 +0300

    First commit

---
+++ home.php
@@ -0,0 +1,199 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['add_to_wishlist'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+
+   $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+   $check_wishlist_numbers->execute([$p_name, $user_id]);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+
+   if($check_wishlist_numbers->rowCount() > 0){
+      $message[] = 'already added to wishlist!';
+   }elseif($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
+      $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
+      $message[] = 'added to wishlist!';
+   }
+
+}
+
+if(isset($_POST['add_to_cart'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+   $p_qty = $_POST['p_qty'];
+   $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+
+   if($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+
+      $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+      $check_wishlist_numbers->execute([$p_name, $user_id]);
+
+      if($check_wishlist_numbers->rowCount() > 0){
+         $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+         $delete_wishlist->execute([$p_name, $user_id]);
+      }
+
+      $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
+      $insert_cart->execute([$user_id, $pid, $p_name, $p_price, $p_qty, $p_image]);
+      $message[] = 'added to cart!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="home-bg">
+
+   <section class="home">
+
+      <div class="content">
+         <span>Fresh Agriculture products which include fruits, vegetables etc. </span>
+         <h3>"Farm Fresh Bounty: Explore Kenya's Vibrant Agri-Fruits and Vegetables!"</h3>
+         <p>Welcome to Agri-Fruits and Vegetables E-commerce Website! We are your one-stop destination for a bountiful assortment of fresh, locally sourced fruits and vegetables from the heart of Kenya. At our online marketplace, we connect farmers and growers with customers, bringing the farm-fresh goodness right to your doorstep.</p>
+         <a href="about.php" class="btn">about us</a>
+      </div>
+
+   </section>
+
+</div>
+
+<section class="home-category">
+
+   <h1 class="title">shop by category</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/cat-1.png" alt="">
+         <h3>fruits</h3>
+         <p>Discover a delightful array of fruits at our e-commerce store, thoughtfully organized into distinct categories. From zesty citrus fruits to the juiciest berries and tropical delights, we've made it easy for you to find your favorites..</p>
+         <a href="category.php?category=fruits" class="btn">Fruits</a>
+      </div>
+
+      <div class="box">
+         <img src="images/cat-3.png" alt="">
+         <h3>vegetables</h3>
+         <p> Explore a variety of leafy greens, crunchy root vegetables, zesty peppers, and more, ensuring you find the perfect ingredients for your culinary creations.</p>
+         <a href="category.php?category=vegetables" class="btn">Vegetables</a>
+      </div>
+
+      <div class="box">
+         <img src="images/cat-5.png" alt="">
+         <h3>Dried Fruits and Vegetables</h3>
+         <p>
+Discover our selection of dried fruits and vegetables, a convenient and nutritious way to enjoy nature's goodness year-round. Whether you're looking for delicious dried apricots for snacking or dehydrated vegetables to enhance your meals.</p>
+         <a href="category.php?category=dried Fruits and Vegetables" class="btn">Dried Fruits and Vegetables</a>
+      </div>
+
+      <div class="box">
+         <img src="images/cat-6.jpg" alt="">
+         <h3>Canned and Frozen</h3>
+         <p>Explore our selection of high-quality canned fruits and vegetables, perfect for quick and easy meal preparations. Additionally, discover our range of frozen fruits and vegetables, ideal for preserving nutritional value and flavor, ensuring you have access to healthy ingredients whenever you need them..</p>
+         <a href="category.php?category=canned and Frozen" class="btn">Canned and Frozen</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="products">
+
+   <h1 class="title">latest products</h1>
+
+   <div class="box-container">
+
+   <?php
+      $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
+      $select_products->execute();
+      if($select_products->rowCount() > 0){
+         while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){ 
+   ?>
+   <form action="" class="box" method="POST">
+      <div class="price">KES <span><?= $fetch_products['price']; ?></span>/-</div>
+      <a href="view_page.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
+      <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+      <div class="name"><?= $fetch_products['name']; ?></div>
+      <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+      <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
+      <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
+      <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
+      <input type="number" min="1" value="1" name="p_qty" class="qty">
+      <input type="submit" value="add to wishlist" class="option-btn" name="add_to_wishlist">
+      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+   </form>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">no products added yet!</p>';
+   }
+   ?>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
